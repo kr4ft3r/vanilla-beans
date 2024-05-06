6bc804a141d8f6bffae8da19cdcae6192c0e0017
Milos Stojanovic <milosh.stoyanovic@gmail.com>
2024-05-07 00:46:49 +0200

    document

--- src/data/fixed-resolution-grid.js
+++ src/data/fixed-resolution-grid.js
@@ -1,7 +1,15 @@
 /**
  * Simple collision-checking grid with evenly distributed number of equal cells.
+ * TODO collision between elements
  */
 class FixedResolutionGrid {
+        /**
+         * 
+         * @param {number} cellSize Length of any side of individual cell in pixels
+         * @param {number} gridWidth Number of cells per row
+         * @param {number} gridHeight Number of cells per column
+         * @param {FixedResolutionGrid~pointCollisionTest} pointCollisionTest Callback for point test
+         */
         constructor(cellSize, gridWidth, gridHeight, pointCollisionTest) {
                 this.cellSize = cellSize;
                 this.width = gridWidth;
@@ -34,6 +42,13 @@ class FixedResolutionGrid {
                         }
                 }
         }
+        /**
+         * Updates object's coordinates in the grid. Call *before* changing the object's position.
+         * @param {Object} value Object being updated
+         * @param {Object} boundingBox {x1,y1,x2,y2} bounding box position BEFORE updating
+         * @param {Object} movement {x,y} vector of object's XY translation that will happen this frame
+         * @returns 
+         */
         update(value, boundingBox, movement) {
                 if (movement.x === 0.0 && movement.y === 0.0) return; // No change
                 
@@ -57,6 +72,11 @@ class FixedResolutionGrid {
                         this.cells[y][x].push(value);
                 //console.log(x+"|"+y);
         }
+        /**
+         * Get all hits at a point, using the provided pointCollisionTest.
+         * @param {Object} point {x,y}
+         * @returns 
+         */
         getAtPoint(point) {
                 const [x, y] = this._getCoordsAtPoint(point.x, point.y);
                 console.log(x+"|"+y+" CHECKING");
@@ -69,3 +89,8 @@ class FixedResolutionGrid {
                 return hits;
         }
 }
+/**
+ * @callback FixedResolutionGrid~pointCollisionTest
+ * @param {Object} object Element of the grid, should probably contain a way to get bounding box
+ * @param {Object} point {x,y} Point of test, {x|0..cellSize*gridWidth}, {y|0..cellSize*gridHeight}
+ */
