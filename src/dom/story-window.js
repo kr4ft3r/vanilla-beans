e0304c51ccb7ae9405f8d4a999a8f158e33bfcb0
Milos Stojanovic <milosh.stoyanovic@gmail.com>
2025-04-02 22:47:31 +0200

    Add settings object and event creation and handling

--- src/dom/story-window.js
+++ src/dom/story-window.js
@@ -1,14 +1,30 @@
 class StoryWindow {
-        constructor(windowElem, textElem, popInCallback, popOutCallback) {
+        /**
+         * 
+         * @param {Element} windowElem 
+         * @param {Element} textElem 
+         * @param {function} popInCallback 
+         * @param {function} popOutCallback 
+         * @param {object} settings 
+         */
+        constructor(windowElem, textElem, popInCallback, popOutCallback, settings) {
                 this.windowElem = windowElem;
                 this.typewriter = new Typewriter(textElem);
                 this.popInCallback = popInCallback;
                 this.popOutCallback = popOutCallback;
                 this.visible = false;
+                this.settings = {...StoryWindow.defaultSettings, ...settings}
+                if (this.settings.handleSkipEvent && Events !== undefined && Events.instance !== undefined) {
+                        Events.instance.createEventOnce('storyWindowSkipRequested');
+                        Events.instance.registerEventListener(this, 'storyWindowSkipRequested');
+                } else if (this.settings.handleSkipEvent) {
+                        console.warn("Handling of events for StoryWindow requested but event system not initialized");
+                }
         }
         
         popIn(data) {
                 this.visible = true;
+                StoryWindow.activeWindow = this;
                 if (this.popInCallback !== null) this.popInCallback(data, this);
         }
         
@@ -18,6 +34,7 @@ class StoryWindow {
         }
         
         write(text) {
+                StoryWindow.activeWindow = this;
                 this.typewriter.write(text);
         }
         
@@ -28,4 +45,19 @@ class StoryWindow {
         skip() {
                 this.typewriter.skip();
         }
+        
+        onStoryWindowSkipRequested() {
+                if (this.settings.handleSkipEventOnlyIfActive && StoryWindow.activeWindow != this) return;
+                if (this.typewriter.running)
+                        this.skip();
+                else if (this.settings.skipAlsoCloses && this.visible)
+                        this.popOut();
+        }
+}
+/** @type {?StoryWindow} */
+StoryWindow.activeWindow = null;
+StoryWindow.defaultSettings = {
+        handleSkipEvent: true,
+        handleSkipEventOnlyIfActive: true,
+        skipAlsoCloses: true,
 }
